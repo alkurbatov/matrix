056fdbf6edae83fc1a4f4b5a8670f35d7ec68b89
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-10-31 13:30:27 +0300

    chore: Better Emacs settings
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,10 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (eval . (let ((root (projectile-project-root)))
-            (setq-local flycheck-cppcheck-standards "c++14")
-            (setq-local flycheck-clang-language-standard "c++14")
-            (setq-local flycheck-clang-include-path
-                        (list (concat root "src")
-			      "/opt/local/include/"))))))
+((c++-mode
+  (flycheck-cppcheck-standards      . "c++14")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-gcc-language-standard   . "c++14")
+  (eval progn (let
+                  ((local-include-paths
+                    (list (concat (projectile-project-root) "src")
+                          "/opt/local/include")))
+                (setq flycheck-clang-include-path local-include-paths)
+                (setq flycheck-gcc-include-path local-include-paths)))))
